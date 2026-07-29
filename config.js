a38d0d39af8a99faca96270a3bcad58fcdc6040e
aodxx <aodaod3826@gmail.com>
2026-07-29 17:05:20 +0800

    Add config.js for Supabase foundation

---
+++ config.js
@@ -0,0 +1,6 @@
+window.LOTTOVIP_CONFIG = Object.freeze({
+  supabaseUrl: "https://walmhfauqpxuysqwmtwd.supabase.co",
+  supabasePublishableKey: "sb_publishable_1iE-nJtzz6rtxEh_be8HMA_Bh-_93bb",
+  siteUrl: "https://aodxx.github.io/LOTTOVIP/"
+});
+
